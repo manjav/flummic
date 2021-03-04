cfd5d544dc4dda6f3c5e34c6136e6fe275df7a82
Mansour <mansurjavadi@gmail.com>
2021-03-04 21:25:58 +0330

    Add localization dependecies

--- islamic/lib/main.dart
+++ islamic/lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'home.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // title: 'Title Example',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('fa', ''),
+      ],
       // debugShowCheckedModeBanner: false,
       // theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
