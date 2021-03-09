c097516b1491f664a8186637960b7b6ffa0664fb
Mansour <mansurjavadi@gmail.com>
2021-03-09 10:37:25 +0330

    Add title for widget

--- islamic/lib/home.dart
+++ islamic/lib/home.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
+  String title = "Sura 1";
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  var textStyle = TextStyle(
+class HomePageState extends State<HomePage> {
       fontFamily: 'Uthmani', fontSize: 22, height: 2, letterSpacing: 2);
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text(
-        AppLocalizations.of(context).helloWorldOn(DateTime.utc(1996, 7, 10)),
-        style: textStyle,
+          centerTitle: true,
+          title: Text(widget.title, style: textStyle),
       ),
     ));
   }
