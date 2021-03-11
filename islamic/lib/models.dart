a08d9e56da3e2877d8ee762d966e87298d4fde80
Mansour <mansurjavadi@gmail.com>
2021-03-11 10:08:43 +0330

    Load and parse uthmani metadata

--- islamic/lib/models.dart
+++ islamic/lib/models.dart
@@ -24,8 +24,68 @@ class Configs {
     for (var p in map["translators"]) instance.translators.add(Person(p));
     instance.reciters = <Person>[];
     for (var p in map["reciters"]) instance.reciters.add(Person(p));
-    
-    instance.translators[0].load(_onCreate, null, null);
+
+    instance.translators[0].load(onQuranLoaded, null, null);
+  }
+
+  static onQuranLoaded() async {
+    var url = "https://grantech.ir/islam/uthmani-meta.ijson";
+    await Loader().load("uthmani-meta.json", url, (String data) {
+      var map = json.decode(data);
+      instance.metadata = QuranMeta();
+      var keys = map.keys;
+      for (var k in keys) {
+        if (k == "suras")
+          for (var c in map[k]) instance.metadata.suras.add(Sura(c));
+        else if (k == "juzes")
+          for (var c in map[k]) instance.metadata.juzes.add(Juz(c));
+        else if (k == "hizbs")
+          for (var c in map[k]) instance.metadata.hizbs.add(Part(c));
+        else if (k == "pages")
+          for (var c in map[k]) instance.metadata.pages.add(Part(c));
+      }
+      _onCreate();
+    }, null, (String e) => print("error: $e"));
+  }
+}
+
+class QuranMeta {
+  List<Sura> suras = <Sura>[];
+  List<Juz> juzes = <Juz>[];
+  List<Part> hizbs = <Part>[];
+  List<Part> pages = <Part>[];
+}
+
+class Sura {
+  int ayas, start, order, rukus, page, type;
+  String name, tname, ename;
+  Sura(s) {
+    ayas = s["ayas"];
+    start = s["start"];
+    order = s["order"];
+    rukus = s["rukus"];
+    page = s["page"];
+    name = s["name"];
+    tname = s["tname"];
+    ename = s["ename"];
+    type = s["type"];
+  }
+}
+
+class Juz extends Part {
+  int page;
+  String name;
+  Juz(j) : super(j) {
+    page = j["page"];
+    name = j["name"];
+  }
+}
+
+class Part {
+  int sura, aya;
+  Part(p) {
+    sura = p["sura"];
+    aya = p["aya"];
   }
 }
 
