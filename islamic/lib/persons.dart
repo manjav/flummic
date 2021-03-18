c9c1eb92d6e8d7bb16cf6f13c5b9b5e8f0124fa8
Mansour <mansurjavadi@gmail.com>
2021-03-18 18:48:06 +0330

    Add Speed dial

--- islamic/lib/persons.dart
+++ islamic/lib/persons.dart
@@ -5,10 +5,16 @@ class PersonPage extends StatefulWidget {
   PersonPageState createState() => PersonPageState();
 }
 
+class PersonPageState extends State<PersonPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController fabAnimController;
 
   @override
   void initState() {
     super.initState();
+    // widget.title = AppLocalizations.of(context).fab_tafsir;
+    fabAnimController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
   Widget build(BuildContext context) {
     return Directionality(
         textDirection: TextDirection.ltr,
@@ -17,5 +23,52 @@ class PersonPage extends StatefulWidget {
             centerTitle: true,
             title: Text(AppLocalizations.of(context).translation_title),
           ),
-          body:Container(),
+          floatingActionButton: SpeedDial(
+            child: AnimatedIcon(
+              icon: AnimatedIcons.add_event,
+              progress: fabAnimController,
+            ),
+            openBackgroundColor: Colors.redAccent,
+            closedBackgroundColor: Colors.redAccent,
+            // closedForegroundColor: Colors.black,
+            // labelsStyle: /* Your label TextStyle goes here */,
+            // controller: /* Your custom animation controller goes here */,
+            onPressed: handleOnPressed,
+            speedDialChildren: <SpeedDialChild>[
+              SpeedDialChild(
+                child: Icon(
+                  Icons.arrow_back,
+                ),
+                foregroundColor: Colors.black,
+                // backgroundColor: Colors.red,
+                label: AppLocalizations.of(context).fab_quran,
+                onPressed: () => speedChildPressed(0),
+              ),
+              SpeedDialChild(
+                child: Icon(Icons.arrow_back),
+                foregroundColor: Colors.black,
+                // backgroundColor: Colors.yellow,
+                label: AppLocalizations.of(context).fab_translate,
+                onPressed: () => speedChildPressed(1),
+              ),
+              SpeedDialChild(
+                child: Icon(Icons.arrow_back),
+                foregroundColor: Colors.black,
+                // backgroundColor: Colors.green,
+                label: AppLocalizations.of(context).fab_tafsir,
+                onPressed: () => speedChildPressed(2),
+              ),
+              //  Your other SpeeDialChildren go here.
+            ],
+          ),
         ));
+  }
+
+  void handleOnPressed(bool isOpen) {
+    setState(() {
+      isOpen ? fabAnimController.forward() : fabAnimController.reverse();
+    });
+  }
+
+  void speedChildPressed(int i) {}
+}
