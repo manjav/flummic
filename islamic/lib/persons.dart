07e4ffd0f6ba19ac829173c8dfc9a98bdb0ee189
Mansour <mansurjavadi@gmail.com>
2021-03-22 21:27:32 +0430

    Localization compatibilies

--- islamic/lib/persons.dart
+++ islamic/lib/persons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:islamic/models.dart';
+import 'package:islamic/localization.dart';
 import 'package:simple_speed_dial/simple_speed_dial.dart';
 
 class PersonPage extends StatefulWidget {
@@ -11,7 +12,6 @@ class PersonPage extends StatefulWidget {
     "mualm_t"
   ];
   static List<String> translationModes = ["quran_t", "trans_t", "tafsi_t"];
-
   String title = "";
   @override
   PersonPageState createState() => PersonPageState();
@@ -29,8 +29,8 @@ class PersonPageState extends State<PersonPage>
   @override
   void initState() {
     super.initState();
-    // widget.title = AppLocalizations.of(context).fab_tafsir;
     isRecitationMode = false;
+    widget.title = (isRecitationMode ? "page_reciter" : "page_translator").l();
     prefsPersons = <String>[];
     prefsPersons.addAll(isRecitationMode ? Prefs.reciters : Prefs.translators);
     configPersons = isRecitationMode
@@ -60,28 +60,7 @@ class PersonPageState extends State<PersonPage>
             automaticallyImplyLeading: false,
           ),
           body: ReorderableListView(
-              children: <Widget>[
-                for (int i = 0; i < prefsPersons.length; i++)
-                  ListTile(
-                    key: Key('$i'),
-                    // tileColor: items[index].isOdd ? oddItemColor : evenItemColor,
-
-                    trailing: CircleAvatar(
-                      backgroundImage: AssetImage('images/icon.png'),
-                    ),
-                    title: Text(
-                      configPersons[prefsPersons[i]].name,
-                    ),
-                    subtitle: Text(
-                      "${configPersons[prefsPersons[i]].mode} ${configPersons[prefsPersons[i]].flag}",
-                    ),
-                    leading: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () =>
-                          removePerson(configPersons[prefsPersons[i]]),
-                    ),
-                  ),
-              ],
+              children: personItems(),
               onReorder: (int oldIndex, int newIndex) {
                 setState(() {
                   if (oldIndex < newIndex) newIndex -= 1;
@@ -103,15 +82,15 @@ class PersonPageState extends State<PersonPage>
             onPressed: handleOnPressed,
             speedDialChildren: <SpeedDialChild>[
               for (int i = 0; i < modes.length; i++)
-              SpeedDialChild(
-                child: Icon(
-                  Icons.arrow_back,
-                ),
-                foregroundColor: Colors.black,
-                // backgroundColor: Colors.red,
-                label: AppLocalizations.of(context).fab_quran,
+                SpeedDialChild(
+                  child: Icon(
+                    Icons.arrow_back,
+                  ),
+                  foregroundColor: Colors.black,
+                  // backgroundColor: Colors.red,
+                  label: modes[i].l(),
                   onPressed: () => speedChildPressed(modes[i]),
-              ),
+                ),
               //  Your other SpeeDialChildren go here.
             ],
           ),
@@ -131,10 +110,34 @@ class PersonPageState extends State<PersonPage>
 
   void speedChildPressed(String mode) {
     Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => PersonListPage(
-              isRecitationMode, mode, prefsPersons, configPersons)));
+        context,
+        MaterialPageRoute(
+            builder: (context) => PersonListPage(
+                isRecitationMode, mode, prefsPersons, configPersons)));
+  }
+
+  List<Widget> personItems() {
+    var items = <Widget>[];
+    for (var t in prefsPersons) {
+      var p = configPersons[t];
+      var subtitle = "${p.mode.l()} ${(p.flag + '_fl').l()}";
+      if (!isRecitationMode) subtitle += " , ${p.size}";
+      items.add(ListTile(
+        key: Key(t),
+        // tileColor: items[index].isOdd ? oddItemColor : evenItemColor,
+
+        trailing: CircleAvatar(
+          backgroundImage: AssetImage('images/icon.png'),
+        ),
+        title: Text(p.name),
+        subtitle: Text(subtitle),
+        leading: IconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () => removePerson(p),
+        ),
+      ));
+    }
+    return items;
   }
 }
 
@@ -219,18 +222,16 @@ class PersonListPageState extends State<PersonListPage> {
 
   Widget personItemBuilder(BuildContext context, int index) {
     var p = persons[index];
+    var subtitle = "${p.mode.l()} ${(p.flag + '_fl').l()}";
+    if (!widget.isRecitationMode) subtitle += " , ${p.size}";
     return GestureDetector(
       onTap: () => selectPerson(p),
       child: ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage('images/icon.png'),
-          ),
-          title: Text(
-            p.name,
-          ),
-          subtitle: Text(
-            "${p.mode} ${p.flag} ${p.size}",
+        leading: CircleAvatar(
+          backgroundImage: AssetImage('images/icon.png'),
         ),
+        title: Text(p.name),
+        subtitle: Text(subtitle),
         trailing: Stack(
           alignment: Alignment.center,
           children: [
@@ -256,7 +257,7 @@ class PersonListPageState extends State<PersonListPage> {
       case PState.waiting:
         p.select(() {
           widget.prefsPersons.add(p.path);
-    Navigator.pop(context);
+          Navigator.pop(context);
           setState(() {});
         }, (double p) {
           setState(() {});
