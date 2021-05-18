2f60ec28e4eeac42d415ca6f88fac5bcf604d908
Mansour <mansurjavadi@gmail.com>
2021-05-18 21:26:32 +0430

    Add loading progressbar for web

--- islamic/lib/pages/web.dart
+++ islamic/lib/pages/web.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:islamic/utils/localization.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebPage extends StatefulWidget {
-  final String? url;
-  WebPage({Key? key, this.url}) : super(key: key);
+  final dynamic? data;
+  WebPage({Key? key, this.data}) : super(key: key);
 
   @override
   _WebPageState createState() => _WebPageState();
 }
 
 class _WebPageState extends State<WebPage> {
+  bool loaded = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: WebView(
+        appBar: AppBar(title: Text(widget.data["title"] ?? "")),
+        body: Stack(alignment: Alignment.center, children: [
+          WebView(
             allowsInlineMediaPlayback: true,
             javascriptMode: JavascriptMode.unrestricted,
             onPageFinished: (String url) {
-              if (url == "http://hidaya.sarand.net/") Navigator.pop(context);
+                if (url == widget.data["url"]) {
+                  loaded = true;
+                  setState(() {});
+                }
             },
-            initialUrl: widget.url));
+              initialUrl: widget.data["url"]),
+          loaded
+              ? SizedBox()
+              : Stack(alignment: Alignment.center, children: [
+                  Container(
+                    width: 240,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      color: Color(0xAA000000),
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(16)),
+                    ),
+                  ),
+                  CircularProgressIndicator(
+                      backgroundColor: Theme.of(context).backgroundColor),
+                  Text("wait_l".l(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.white))
+                ])
+        ]));
   }
 }
