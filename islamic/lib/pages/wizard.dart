5fad24afcf75303eff2aee8ae6a9bc55af59b930
Mansour <mansurjavadi@gmail.com>
2021-06-10 11:16:51 +0430

    Add page layout

--- islamic/lib/pages/wizard.dart
+++ islamic/lib/pages/wizard.dart
@@ -18,5 +18,40 @@ class _WizardPageState extends State<WizardPage> {
 
   @override
   Widget build(BuildContext context) {
+    _app = MyApp.of(context);
+    _theme = Theme.of(context);
+    var r = Localization.isRTL;
+    return Scaffold(
+        body: Stack(children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[_header(), _divider(), _slides()],
+      ),
+    ]));
+  }
+
+  Widget _header() {
+    return Container(
+        height: 200);
+  }
+
+  Widget _divider() {
+    return Container(
+        height: 4,
+        );
+  }
+
+  Widget _slides() {
+    return Expanded(
+    );
+  }
+
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+          Center(child: FlutterLogo(size: 200.0))
+      ],
+    );
   }
 }
