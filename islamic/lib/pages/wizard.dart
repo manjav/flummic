b5914df57e49ffabc92ab46841db1395ca72548c
Mansour <mansurjavadi@gmail.com>
2021-06-10 11:13:27 +0430

    Add epty wizard

---
+++ islamic/lib/pages/wizard.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islamic/main.dart';
+import 'package:islamic/utils/localization.dart';
+
+class WizardPage extends StatefulWidget {
+  WizardPage({Key? key}) : super(key: key);
+
+  @override
+  _WizardPageState createState() => _WizardPageState();
+}
+
+class _WizardPageState extends State<WizardPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
